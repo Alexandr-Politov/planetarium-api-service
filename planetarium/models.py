8984de886bb64b42dda5197f9d04e43f9af6db07
Alexandr Politov <apolitov83@gmail.com>
2024-10-10 17:08:27 +0300

    Models created for User(AbstractUser) and for 'planetarium' app.

--- planetarium/models.py
+++ planetarium/models.py
@@ -1,3 +1,90 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return f"{self.name}: {self.rows} rows x {self.seats_in_row} seats"
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(
+        AstronomyShow,
+        on_delete=models.CASCADE,
+        related_name="sessions",
+    )
+    planetarium_dome = models.ForeignKey(
+        PlanetariumDome,
+        on_delete=models.CASCADE,
+        related_name="sessions",
+    )
+    show_time = models.DateTimeField()
+
+    class Meta:
+        ordering = ["-show_time"]
+
+    def __str__(self):
+        return f"{self.astronomy_show.title} {str(self.show_time)}"
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return str(self.created_at)
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    show_session = models.ForeignKey(
+        ShowSession,
+        on_delete=models.CASCADE,
+        related_name="tickets",
+    )
+    reservation = models.ForeignKey(
+        Reservation,
+        on_delete=models.CASCADE,
+        related_name="tickets",
+    )
+
+    class Meta:
+        ordering = ["row", "seat"]
+        constraints = [models.UniqueConstraint(
+            fields=["row", "seat", "show_session"],
+            name="unique_ticket",
+        )]
