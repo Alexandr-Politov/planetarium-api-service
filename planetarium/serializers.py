9c15d77b5a48f8c2c225696231f6ab7d51860d8f
Alexandr Politov <apolitov83@gmail.com>
2024-10-13 11:36:54 +0300

    Reservation serializer updated (added method create) to create tickets as a content of new reservation

--- planetarium/serializers.py
+++ planetarium/serializers.py
@@ -67,7 +67,14 @@ class TicketSerializer(serializers.ModelSerializer):
 
 
 class ReservationSerializer(serializers.ModelSerializer):
-    tickets = TicketSerializer(many=True)
+    tickets = TicketSerializer(many=True, read_only=False)
     class Meta:
         model = Reservation
         fields = ["id", "created_at", "tickets"]
+
+    def create(self, validated_data):
+        tickets_data = validated_data.pop("tickets")
+        reservation = Reservation.objects.create(**validated_data)
+        for ticket_data in tickets_data:
+            Ticket.objects.create(reservation=reservation, **ticket_data)
+        return reservation
